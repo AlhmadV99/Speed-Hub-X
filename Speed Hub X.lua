78a5c034e5b03021c210b1875c5fa2cdfd77ba5c
AlhmadV99 <ahmadv99scripts@gmail.com>
2025-08-01 20:02:02 +0800

    Update Speed Hub X.lua

--- Speed Hub X.lua
+++ Speed Hub X.lua
@@ -1,2 +1,3 @@
---loadstring(game:HttpGet("https://raw.githubusercontent.com/AhmadV99/Speed-Hub-X/main/Speed%20Hub%20X.lua", true))()
-loadstring(game:HttpGet("https://pastefy.app/xaY524Sm/raw"))()
+loadstring(game:HttpGet("https://pastefy.app/psn6jtT3/raw"))()
+
+--loadstring(game:HttpGet("https://pastefy.app/xaY524Sm/raw"))()
